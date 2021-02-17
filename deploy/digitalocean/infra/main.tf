bf6cea68e56effb688b25720fdf8a1eed374d35d
fpignataro <>
2021-02-17 13:27:44 -0300

    quito fw

--- deploy/digitalocean/infra/main.tf
+++ deploy/digitalocean/infra/main.tf
@@ -9,7 +9,7 @@ resource "digitalocean_tag" "environment" {
 
 # Lookup image to get id
 data "digitalocean_image" "official" {
-  count = var.custom_image == true ? 0 : 1
+  #count = var.custom_image == true ? 0 : 1
   slug  = var.image_name
 }
 
@@ -31,47 +31,4 @@ resource "digitalocean_droplet" "main" {
     digitalocean_tag.tenant.id,
     digitalocean_tag.environment.id
   ]
-}
-
-#Module     : Firewall for RTPENGINE
-resource "digitalocean_firewall" "fw_rtpengine" {
-  name = var.name_rtpengine
-
-  droplet_ids = [module.droplet_rtpengine.id[0]]
-
-  inbound_rule {
-    protocol         = "tcp"
-    port_range       = "22"
-    source_addresses = ["0.0.0.0/0"]
-  }
-
-  inbound_rule {
-    protocol            = "tcp"
-    port_range          = "22222"
-    source_droplet_ids  = [module.droplet_omlapp.id[0]]
-  }
-
-  inbound_rule {
-    protocol         = "udp"
-    port_range       = "20000-50000"
-    source_addresses = ["0.0.0.0/0"]
-  }
-
-
-  outbound_rule {
-    protocol              = "tcp"
-    port_range            = "1-65535"
-    destination_addresses = ["0.0.0.0/0", "::/0"]
-  }
-
-  outbound_rule {
-    protocol              = "udp"
-    port_range            = "1-65535"
-    destination_addresses = ["0.0.0.0/0", "::/0"]
-  }
-
-  outbound_rule {
-  protocol              = "icmp"
-  destination_addresses = ["0.0.0.0/0", "::/0"]
-}
 }
